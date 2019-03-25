ee3137b7f7548f37f7a99b0599736292e86b0b38
cesar <cesarpantoj@gmail.com>
2019-03-25 14:30:36 +0000

    - Search for aws instances using wildcards: *kafka* and *zookeper* - Corrected command to remove addresses in Linux - Formatting changes

--- Instance.py
+++ Instance.py
@@ -1,4 +1,7 @@
+from functools import reduce
+
 import boto3
+
 
 class Instance:
     def __init__(self, name, ip, port):
@@ -6,35 +9,43 @@ class Instance:
         self.ip = ip
         self.port = port
 
-class RetrieveInstanceIPs():
-    def getIps(self,service,port):
+
+class RetrieveInstanceIPs:
+    def getIps(self, service, port):
         raise NotImplementedError("Subclass must implement abstract method")
 
+
 class ManualInstances(RetrieveInstanceIPs):
-    def getIps(self,service,ips,port):
-        instances=[]
+    def getIps(self, service, ips, port):
+        instances = []
         for ip in ips.split(','):
-            instances.append(Instance(name=service,ip=ip,port=port))
+            instances.append(Instance(name=service, ip=ip, port=port))
         return instances
 
+
 class AWSInstances(RetrieveInstanceIPs):
-    def __init__(self,profile,region):
+    def __init__(self, profile, region):
         self.profile = profile
         self.region = region
         self.session = boto3.Session(profile_name=profile, region_name=region)
-    def getIps(self,service,port):
-        instances=[]
+
+    def getIps(self, service, port):
+        instances = []
         for ec2_ip in self.req_aws_ips(service, self.region):
-            instances.append(Instance(name=service,ip=ec2_ip,port=port))
+            instances.append(Instance(name=ec2_ip[1], ip=ec2_ip[0], port=port))
         return instances
-    def req_aws_ips(self,service, region):
-        ips=[]
-        aws_filter = lambda name,value: [{'Name':'tag:'+name,'Values':[value]}]
+
+    def req_aws_ips(self, service, region):
+        ips = []
+        aws_filter = lambda name, value: [{'Name': 'tag:' + name, 'Values': [value]}]
         client = self.session.client('ec2')
-        response = client.describe_instances(Filters=aws_filter('Name',service))
+        response = client.describe_instances(Filters=aws_filter('Name', service))
         for res in response.get('Reservations'):
             for instance in res.get('Instances'):
                 ip = instance.get(u'PrivateIpAddress')
                 if ip is not None:
-                  ips.append(instance.get(u'PrivateIpAddress'))
+                    namesmap = list(filter(lambda e: e.get(u'Key') == u'Name', instance.get(u'Tags')))
+                    names = list(map(lambda e: e.get(u'Value'), namesmap))
+                    name = reduce(lambda a, b: a + " " + b, names)
+                    ips.append((instance.get(u'PrivateIpAddress'), name))
         return ips
--- kafkatunnel.py
+++ kafkatunnel.py
@@ -5,45 +5,50 @@ import subprocess
 
 from Instance import ManualInstances, AWSInstances
 
+
 @click.group(help='Access kafka/zookeeper via ssh tunnel to consume and produce messages from your local machine')
 def cli():
     pass
 
-@cli.command(help='retrieve kafka/zookeeper ip\'s from AWS (important: a resource tag with Name=kafka/zookeeper is needed)')
+
+@cli.command(
+    help='retrieve kafka/zookeeper ip\'s from AWS (important: a resource tag with Name=kafka/zookeeper is needed)')
 @click.argument('jump_host')
-@click.option('-zp','--zookeeper_port',default='2181')
-@click.option('-kp','--kafka_port',default='9092')
-@click.option('-r','--region',default='ap-southeast-2')
-@click.option('-p','--profile',default='default')
-def aws(jump_host,zookeeper_port,kafka_port,region,profile):
-    instances=[]
-    click.echo(' * retrieving ip\'s from AWS ({},{}) zookeeper/kafka ec2 instances by tag_name ...'.format(profile,region))
-    aws = AWSInstances(profile,region)
-    instances += aws.getIps('zookeeper',zookeeper_port)
-    instances += aws.getIps('kafka',kafka_port)
-    connect(jump_host,instances)
+@click.option('-zp', '--zookeeper_port', default='2181')
+@click.option('-kp', '--kafka_port', default='9092')
+@click.option('-r', '--region', default='ap-southeast-2')
+@click.option('-p', '--profile', default='default')
+def aws(jump_host, zookeeper_port, kafka_port, region, profile):
+    instances = []
+    click.echo(
+        ' * retrieving ip\'s from AWS ({},{}) zookeeper/kafka ec2 instances by tag_name ...'.format(profile, region))
+    aws = AWSInstances(profile, region)
+    instances += aws.getIps('*zookeeper*', zookeeper_port)
+    instances += aws.getIps('*kafka*', kafka_port)
+    connect(jump_host, instances)
 
 
 @cli.command(help='provide the IP\'s of your zookeeper/kafka')
 @click.argument('jump_host')
 @click.argument('zookeeper_ips')
 @click.argument('kafka_ips')
-@click.option('-zp','--zookeeper_port',default='2181')
-@click.option('-kp','--kafka_port',default='9092')
-def manual(jump_host,zookeeper_ips, kafka_ips, zookeeper_port, kafka_port):
-    instances=[]
+@click.option('-zp', '--zookeeper_port', default='2181')
+@click.option('-kp', '--kafka_port', default='9092')
+def manual(jump_host, zookeeper_ips, kafka_ips, zookeeper_port, kafka_port):
+    instances = []
     click.echo(' * using manual ip\'s ...')
     man = ManualInstances()
-    instances += man.getIps('zookeeper',zookeeper_ips, zookeeper_port)
-    instances += man.getIps('kafka',kafka_ips, kafka_port)
-    connect(jump_host,instances)
+    instances += man.getIps('zookeeper', zookeeper_ips, zookeeper_port)
+    instances += man.getIps('kafka', kafka_ips, kafka_port)
+    connect(jump_host, instances)
 
 
-def connect(jump_host,instances):
+def connect(jump_host, instances):
     print_instances(instances)
     add_local_interfaces(instances)
-    connect_ssh_tunnel(jump_host,instances)
+    connect_ssh_tunnel(jump_host, instances)
     remove_local_interfaces(instances)
+
 
 def add_local_interfaces(instances):
     click.echo(' * adding interface, user password might be needed')
@@ -54,27 +59,31 @@ def add_local_interfaces(instances):
             cmd = ['sudo', 'ip', 'add', 'a', 'dev', 'lo', instance.ip]
         subprocess.call(cmd)
 
+
 def remove_local_interfaces(instances):
-    click.echo(' * removing interface, user/root password might be needed')
+    click.echo(' * removing interfaces, user/root password might be needed')
     for instance in instances:
         if sys.platform == 'darwin':
-           cmd = ['sudo', 'ifconfig', 'lo0', '-alias', instance.ip]
+            cmd = ['sudo', 'ifconfig', 'lo0', '-alias', instance.ip]
         else:
-            cmd = ['sudo', 'ip', 'del', 'a', 'dev', 'lo', instance.ip]
+            cmd = ['sudo', 'ip', 'a', 'del', instance.ip+"/32", 'dev', 'lo']
         subprocess.call(cmd)
+
 
 def print_instances(instances):
     click.echo('')
     for i in instances:
-        click.echo('{:<10} on {:<15} port {:>5}'.format(i.name,i.ip,i.port))
+        click.echo('{:<10} on {:<15} port {:>5}'.format(i.name, i.ip, i.port))
     click.echo('')
 
-def connect_ssh_tunnel(jump_host,instances):
+
+def connect_ssh_tunnel(jump_host, instances):
     click.echo(' * connecting to jump host ' + jump_host)
     opts = []
     for i in instances:
-        opts += ['-L','{ip}:{port}:{ip}:{port}'.format(ip=i.ip,port=i.port)]
+        opts += ['-L', '{ip}:{port}:{ip}:{port}'.format(ip=i.ip, port=i.port)]
     subprocess.call(['ssh'] + opts + [jump_host])
+
 
 if __name__ == '__main__':
     cli()
